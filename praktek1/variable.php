74d2dafda0f5bcf54298edf786dd0519cb2cfb72
hikaaam <36694324+hikaaam@users.noreply.github.com>
2018-02-21 17:20:47 +0700

    Add files via upload

---
+++ praktek1/variable.php
@@ -0,0 +1,13 @@
+<?php
+$nama = "ilyas";
+$umur = 19;
+$honor = 69.68;
+$statusmenikah = false;
+var_dump($nama); echo"<br>";
+var_dump($umur); echo"<br>";
+var_dump($honor); echo"<br>";
+var_dump($statusmenikah); echo"<br>";
+
+$statusmenikah = "menikah";
+var_dump($statusmenikah); echo "<dbr>";
+?>
