7c6091b50556ab045fffce7ef2039013409cb0a6
hikaaam <36694324+hikaaam@users.noreply.github.com>
2018-02-28 16:46:42 +0700

    Add files via upload

---
+++ praktek2/array-multi.php
@@ -0,0 +1,18 @@
+<?php
+$data = array(
+"web-prog" => array(
+    "tamami" => 7,
+    "aldi" => 8,
+    "eko" => 6
+),
+"kalkulus" =>array(
+    "tamami" => 9,
+    "eko"   => 8,
+    "aldi" => 7
+    )
+);
+
+echo "nilai web prog aldi : " . $data["web-prog"]["aldi"]."<br>";
+echo "nilai kalkulus aldi : " .$data["kalkulus"]["aldi"]."<br>";
+
+?>
